3871ef972b1ba3cbac5f745ce7c1a86bd12ef52c
romanemarteaubazouni <romane.marteaubazouni@gmail.com>
2026-02-02 10:34:19 +0100

    3 first exercices

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,47 @@
 #include <sil/sil.hpp>
+#include <iostream>
+
+void green(sil::Image& image) // Ne gardez que le vert
+{
+    for (glm::vec3& color : image.pixels())
+    {
+        color.r = 0.f;
+        color.b = 0.f;
+    }
+}
+
+void swapRedBlue(sil::Image& image) // Echangez les canaux (rouge <--> bleu)
+{
+    for (glm::vec3& color : image.pixels())
+    {
+        std::swap(color.r, color.b);
+    }
+}
+
+void blackAndWhite(sil::Image& image) // Image en N&B
+{
+    for (glm::vec3& color : image.pixels())
+    {
+        float grey = 0.3f * color.r + 0.59f * color.g + 0.11f * color.b;
+        color = glm::vec3(grey);
+    }
+}
 
 int main()
 {
-    sil::Image image{"images/logo.png"};
-    // TODO: modifier l'image
-    image.save("output/pouet.png");
+    {
+        sil::Image image{"images/logo.png"}; // Lis l'image
+        green(image); // Utilise la fonction pour modifier l'image
+        image.save("output/green_imac.png"); // Sauvegarde l'image
+    }
+    {
+        sil::Image image{"images/logo.png"};
+        swapRedBlue(image);
+        image.save("output/swaped_imac.png");
+    }
+    {
+        sil::Image image{"images/logo.png"};
+        blackAndWhite(image);
+        image.save("output/n&b_imac.png");
+    }
 }
